49eea6f5140ded56b60176bcd1441f621a820f1d
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2018-01-11 20:21:29 -0600

    Started the new Earth implementation

--- fufilGardenerRequestBot/Earth.java
+++ fufilGardenerRequestBot/Earth.java
@@ -1,4 +1,19 @@
-package planet;
+import java.util.HashMap;
 
 public class Earth {
+
+    public static HashMap<Integer, Task> earthTaskMap = new HashMap<>();
+    public static HashMap<Integer, Task> earthAttackTargetsMap = new HashMap<>();
+    public static HashMap<Integer, Task> earthProduceRobotMap = new HashMap<>();
+
+    public static HashMap<Integer, Blueprint> earthBlueprintMap = new HashMap<>();
+    public static HashMap<Integer, Rocket> earthRocketMap = new HashMap<>();
+    public static HashMap<Integer, Robot> earthWorkerMap = new HashMap<>();
+    public static HashMap<Integer, Factory> earthFactoryMap = new HashMap<>();
+    public static HashMap<Integer, Robot> earthAttackerMap = new HashMap<>();
+
+    public void execute() {
+        // Execute blueprints, Rockets, Workers, Factories, Attackers
+        
+    }
 }
