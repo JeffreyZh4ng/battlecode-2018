7d7db3cad327f4ae323c0d85b619428e4a724dfc
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2018-01-12 01:55:31 -0600

    Created a method that will remove all dead units from all maps at the beginning of each round. Still needs to be tested.

--- fufilGardenerRequestBot/planets/Earth.java
+++ fufilGardenerRequestBot/planets/Earth.java
@@ -8,7 +8,9 @@ import structures.Blueprint;
 import structures.Factory;
 import structures.Rocket;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Earth {
     public static HashMap<Integer, Task> earthTaskMap = new HashMap<>();
@@ -22,7 +24,7 @@ public class Earth {
     public static HashMap<Integer, Robot> earthAttackerMap = new HashMap<>();
 
     public void execute() {
-        removeDeadUnits();
+        updateDeadUnits();
 
         // Execute blueprints, Rockets, Workers, Factories, Attackers
         for (int blueprintId: earthBlueprintMap.keySet()) {
@@ -34,12 +36,41 @@ public class Earth {
      * Since the method has not yet been implemented in the API, we must manually check if any unit died
      * last round
      */
-    private void removeDeadUnits() {
+    private void updateDeadUnits() {
+        HashSet<Integer> unitSet = new HashSet<>();
         VecUnit units = Globals.gameController.myUnits();
-        for (int blueprintId: earthBlueprintMap.keySet()) {
-
+        for (int i = 0; i < units.size(); i++) {
+            unitSet.add(units.get(i).id());
         }
+
+        earthBlueprintMap = findDeadUnits(unitSet, earthBlueprintMap);
+        earthRocketMap = findDeadUnits(unitSet, earthRocketMap);
+        earthWorkerMap = findDeadUnits(unitSet, earthWorkerMap);
+        earthFactoryMap = findDeadUnits(unitSet, earthFactoryMap);
+        earthAttackerMap = findDeadUnits(unitSet, earthAttackerMap);
     }
 
+    /**
+     * Helper method for the updateDeadUnits method. This method will compile an array all units in the specified
+     * HashMap but not in the units list returned by Globals.gameController.myUnits(). Will then remove all the
+     * units specified by the array and remove them from the map
+     * @param unitSet The set of units returned by the Game Controller
+     * @param searchMap The current map you are purging
+     * @param <T> The value of the map
+     * @return A new map without the dead units
+     */
+    private <T> HashMap<Integer, T> findDeadUnits(HashSet<Integer> unitSet, HashMap<Integer, T> searchMap) {
+        ArrayList<Integer> deadUnits = new ArrayList<>();
+        for (int unitId: searchMap.keySet()) {
+            if (!unitSet.contains(unitId)) {
+                deadUnits.add(unitId);
+            }
+        }
 
+        for (int unitId: deadUnits) {
+            searchMap.remove(unitId);
+        }
+
+        return searchMap;
+    }
 }
