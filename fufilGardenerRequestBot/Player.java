b61504365b5af314b141492a245995fe140c4cac
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2018-01-12 00:57:30 -0600

    Moved methods from the Player class to the robot class because you cant access any variable or methods from the default package.

--- fufilGardenerRequestBot/Player.java
+++ fufilGardenerRequestBot/Player.java
@@ -8,7 +8,6 @@ import robots.Worker;
 public class Player {
 
     private static final int FLOOD_ROUND = 750;
-    
 
     public static void main(String[] args) {
 
@@ -39,48 +38,4 @@ public class Player {
             Earth.earthWorkerMap.put(unitId, worker);
         }
     }
-
-    /**
-     * Method that will return a random direction when called. Intended to be used for preliminary testing
-     * @return One of 8 random directions
-     */
-    public static Direction pickRandomDirection() {
-        int randomInt = (int)(Math.random()*8 + 1);
-        return Direction.swigToEnum(randomInt);
-    }
-
-    /**
-     * Subject to change. The isOccupiable method is bugged in the API
-     * @param robotId The id of the robot
-     * @return An available direction. Null if none are available
-     */
-    public static Direction returnAvailableDirection(int robotId) {
-        for (int i = 0; i < 8; i++) {
-            if (Globals.gameController.canMove(robotId, Direction.swigToEnum(i))) {
-                return Direction.swigToEnum(i);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * finds next optimal locations for each robot to move to and moves them to that location
-     */
-    public static void moveWorkers() {
-        //find optimal next locations, consider if robot in path is moving, find optimal order of execution
-        //execute move actions
-    }
-
-    //    /**
-//     * Iterates through map locations and prints karbonite values for all locations
-//     */
-//    public static void printKarboniteValues() {
-//        long width = gameController.startingMap(Planet.Earth).getWidth();
-//        long height = gameController.startingMap(Planet.Earth).getHeight();
-//        for (int x = 0; x < width; x++) {
-//            for (int y = 0; y < height; y++) {
-//                System.out.println(gameController.startingMap(Planet.Earth).initialKarboniteAt(new MapLocation(Planet.Earth, x, y)));
-//            }
-//        }
-//    }
 }
--- fufilGardenerRequestBot/robots/Robot.java
+++ fufilGardenerRequestBot/robots/Robot.java
@@ -1,5 +1,7 @@
 package robots;
 
+import bc.Direction;
+import commandsAndRequests.Globals;
 import commandsAndRequests.Task;
 
 import java.util.PriorityQueue;
@@ -27,6 +29,49 @@ public abstract class Robot {
      */
     public boolean sendRequestToFactory() {
         return true;
+    }
+
+//    /**
+//     * Method that will return a random direction when called. Intended to be used for preliminary testing
+//     * @return One of 8 random directions
+//     */
+//    public static Direction pickRandomDirection() {
+//        int randomInt = (int)(Math.random()*8 + 1);
+//        return Direction.swigToEnum(randomInt);
+//    }
+//
+//    /**
+//     * Subject to change. The isOccupiable method is bugged in the API
+//     * @param robotId The id of the robot
+//     * @return An available direction. Null if none are available
+//     */
+//    public static Direction returnAvailableDirection(int robotId) {
+//        for (int i = 0; i < 8; i++) {
+//            if (Globals.gameController.canMove(robotId, Direction.swigToEnum(i))) {
+//                return Direction.swigToEnum(i);
+//            }
+//        }
+//        return null;
+//    }
+//    /**
+//     * Iterates through map locations and prints karbonite values for all locations
+//     */
+//    public static void printKarboniteValues() {
+//        long width = gameController.startingMap(Planet.Earth).getWidth();
+//        long height = gameController.startingMap(Planet.Earth).getHeight();
+//        for (int x = 0; x < width; x++) {
+//            for (int y = 0; y < height; y++) {
+//                System.out.println(gameController.startingMap(Planet.Earth).initialKarboniteAt(new MapLocation(Planet.Earth, x, y)));
+//            }
+//        }
+//    }
+
+    /**
+     * finds next optimal locations for each robot to move to and moves them to that location
+     */
+    public static void moveWorkers() {
+        // find optimal next locations, consider if robot in path is moving, find optimal order of execution
+        // execute move actions
     }
 
     /**
