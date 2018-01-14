5f63a40dfc193952de6c2d661a1cea4aa3ee1b0a
KyungSun4 <john.han7@yahoo.com>
2018-01-14 00:54:57 -0600

    outlined a few more methods not yet implemented

--- fufilGardenerRequestBot/units/robots/Ranger.java
+++ fufilGardenerRequestBot/units/robots/Ranger.java
@@ -11,7 +11,7 @@ public class Ranger extends Robot {
     public Ranger(int id) {
         super(id);
     }
-    
+
     public void run() {
         System.out.println("Got to run method! Task queue size: " + this.robotTaskQueue.size());
         if (this.emergencyTask != null) {
@@ -45,12 +45,50 @@ public class Ranger extends Robot {
                 System.out.println("Running MOVE!");
                 return move(this.id, commandLocation);
             case IN_COMBAT:
-                //return attack(commandLocation)
+                //return fight()
             case SNIPE:
                 //return snipe(commandLocation);
             default:
                 return true;
         }
+    }
+
+    /**
+     * attacks enemy robots within its range, or move towards nearest one
+     * @return if it has an attack objective
+     */
+    private boolean fight() {
+        return true;
+    }
+
+    /**
+     * attacks with specific enemy unit and location in mind, a more aggressive strategy
+     * @param targetLocation where enemy unit is thought to be
+     * @param targetId the id of targeted enemy unit
+     * @return whether or enemy robot was killed yet
+     */
+    private boolean targetedAttack(MapLocation targetLocation, int targetId) {
+        return true;
+    }
+
+    /**
+     * attacks the weakest enemy robot that it can attack
+     * @return whether or not a robot was attacked
+     */
+    private boolean attackWeakestEnemyRobot() {
+        return true;
+    }
+
+
+    /**
+     * snipes location, does whole proccess and returns true once location is sniped
+     * @param snipeLocation location to snipe
+     * @return if compleated this round returns true otherwise returns false
+     */
+    private boolean snipe(MapLocation snipeLocation) {
+
+        //snipeing ist that great so will implement latter
+        return true;
     }
 
 }
